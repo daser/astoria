febd1bfa69aee1c9c9a049ecf35ce9eead7ad972
Daser David <daseronline@gmail.com>
2013-06-02 08:21:46 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Astoria::Application.config.secret_token = '4da6943c18b6434c14d3c4e7dbad0ff78a16ca604653249a1593bff0b47272f30a72af306976853b14a8987b5679b4234d42773008743ed760bdd130c48f098d'
